f45d0f72eddf14c7916e2c572da11ccfa5beb80d
shinya852 <shinya.june@gmail.com>
2022-02-15 15:18:39 +0900

    compleate app & start

--- app/app3.py
+++ app/app3.py
@@ -6,12 +6,16 @@ def dispPhoto(path):
     # 画像を読み込む
     newImage = PIL.Image.open(path).resize((300,300))
     # labelに表示する
-    imageData = PIL.ImageTK.PhotoImage(newImage)
+    # error
+    #imageData = PIL.ImageTK.PhotoImage(newImage)
+    imageData = PIL.ImageTk.PhotoImage(newImage)
     imageLabel.configure(image = imageData)
     imageLabel.image = imageData
 def openFile():
     fpath = fd.askopenfilename()
     if fpath:
+        #erro
+        #dispPhoto(fpath)
         dispPhoto(fpath)
 
 root = tk.Tk()
