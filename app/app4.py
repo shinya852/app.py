bfd94924081b5c2386910c65aa13db787c0c641d
shinya852 <shinya.june@gmail.com>
2022-02-28 20:36:14 +0900

    create app4

---
+++ app/app4.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+import tkinter.filedialog as fd
+import PIL.Image
+import PIL.ImageTk
+def dispPhoto(path):
+    # 画像を読み込む 灰色に変換する
+    newImage = PIL.Image.open(path).convert("L").resize((300,300))
+    # labelに表示
+    # error
+    #imageData = PIL.ImageTK.PhotoImage(newImage)
+    imageData = PIL.ImageTk.PhotoImage(newImage)
+    imageLabel.configure(image = imageData)
+    imageLabel.image = imageData
+def openFile():
+    fpath = fd.askopenfilename()
+    if fpath:
+        #erro
+        #dispPhoto(fpath)
+        dispPhoto(fpath)
+
+root = tk.Tk()
+root.geometry("400x350")
+
+btn = tk.Button(text="ファイルを開く", command = openFile)
+imageLabel = tk.Label()
+btn.pack()
+imageLabel.pack()
+tk.mainloop()
