a73b471c3abb23f5d633587994bed3938eee7ae7
shinya852 <shinya.june@gmail.com>
2022-03-15 11:05:26 +0900

    compleate textbox next add excel

--- app/app.practice.py
+++ app/app.practice.py
@@ -3,7 +3,7 @@ import tkinter
 
 class Application(tkinter.Frame):#フレームのクラスを作成継承
     def __init__(self,root=None):#処理 受けとる(基底クラス=Frameのイニシャライザーを呼ぶ)
-        super().__init__(root, width=100, height=100,#横380　縦200
+        super().__init__(root, width=380, height=280,#横幅,　縦幅
                          borderwidth=4, relief='groove')#境界線の太さ4　境界線の種類groove
         self.root = root#classの別メソッドでも使用する為インスタンス変数に代入
         self.pack()#位置の設定
@@ -17,7 +17,25 @@ class Application(tkinter.Frame):#フレームのクラスを作成継承
         quit_btn["text"] = '閉じる' #ボタンに表示させる文字 ＝ '閉じる'
         quit_btn["command"] = self.root.destroy #ボタンを押した際の命令　＝　アプリを閉じる
         quit_btn.pack(side='bottom')
-        
+
+        #テキストボックスの作成
+        self.text_box = tkinter.Entry(self)
+        self.text_box['width'] = 10
+        self.text_box.pack()
+
+        #実行ボタン
+        submit_btn = tkinter.Button(self)
+        submit_btn["text"] = "実行"
+        submit_btn["command"] = self.input_handler
+        submit_btn.pack()
+
+        #メッセージの出力
+        self.message = tkinter.Message(self)
+        self.message.pack()
+
+    def input_handler(self):#値の習得
+        text = self.text_box.get()
+        self.message["text"] = text + '!!'#習得したtextに文字列"!!"を結合
 
         
 root = tkinter.Tk() #作成したオブジェクトをrootに代入Tkのスペル注意
